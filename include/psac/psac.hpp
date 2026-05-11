d41a6070e7fc7334ccb469e01e73f2fab0e3a1a8
Daniel Brommer <daniel.brommer@student.kit.edu>
2026-05-11 09:26:02 +0200

    feat: add sa computation, add result struct to have computations as free functions

--- include/psac/psac.hpp
+++ include/psac/psac.hpp
@@ -1,57 +1,47 @@
 #pragma once
 
 #include <mpi.h>
-
-#include <fstream>
-#include <iostream>
-#include <string>
 #include <vector>
-
 #include <alphabet.hpp>
-#include <check_suffix_array.hpp>
-#include <check_suffix_tree.hpp>
 #include <suffix_array.hpp>
 #include <suffix_tree.hpp>
-
-#include <mxx/comm.hpp>
-#include <mxx/env.hpp>
-#include <mxx/file.hpp>
-#include <mxx/timer.hpp>
 #include <mxx/utils.hpp>
 
-struct sa_builder {
-public:
-    sa_builder() = default;
-
-    void construct_sa_lcp(const MPI_Comm& mpi_com, std::string& local_str, std::size_t resolval_threshold) {
-        mxx::comm comm = mxx::comm(mpi_com);
-        mxx::print_node_distribution(comm);
-
-        mxx::timer t;
-        double start = t.elapsed();
-
-        // construct SA+LCP
-        using index_t = uint64_t;
-        suffix_array<char, index_t, true> sa(comm);
-        sa.construct(local_str.begin(), local_str.end(), resolval_threshold);
-        double end = t.elapsed() - start;
-        if (comm.rank() == 0) {
-            std::cerr << "PSAC time: " << end << " ms" << std::endl;
-        }
-
-        sa_vec = sa.local_SA;
-        lcp_vec = sa.local_LCP;
-    }
-
-    std::vector<unsigned long>& get_sa() {
-        return sa_vec;
-    }
-
-    std::vector<unsigned long>& get_lcp() {
-        return lcp_vec;
-    }
-
-private:
-    std::vector<unsigned long> sa_vec;
-    std::vector<unsigned long> lcp_vec;
+namespace psac {
+template <typename return_t = unsigned long>
+struct sa_result {
+    std::vector<return_t>                 sa;
+    std::optional<std::vector<return_t>>  lcp; // present only if build_sa_lcp was called
 };
+
+template <typename return_t = unsigned long, typename Container>
+[[nodiscard]] sa_result<return_t>
+build_sa(MPI_Comm mpi_comm,
+         const Container& input,
+         std::size_t resolution_threshold)
+{
+    mxx::comm comm{mpi_comm};
+    mxx::print_node_distribution(comm);
+
+    suffix_array<typename Container::value_type, return_t> sa{comm};
+    sa.construct(input.begin(), input.end(), resolution_threshold);
+
+    return { std::move(sa.local_SA), std::nullopt };
+}
+
+template <typename return_t = unsigned long, typename Container>
+[[nodiscard]] sa_result<return_t>
+build_sa_lcp(MPI_Comm mpi_comm,
+             const Container& input,
+             std::size_t resolution_threshold)
+{
+    mxx::comm comm{mpi_comm};
+    mxx::print_node_distribution(comm);
+
+    suffix_array<typename Container::value_type, return_t, /*_CONSTRUCT_LCP=*/true> sa{comm};
+    sa.construct(input.begin(), input.end(), resolution_threshold);
+
+    return { std::move(sa.local_SA), std::move(sa.local_LCP) };
+}
+
+} // namespace psac
